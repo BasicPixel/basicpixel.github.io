cca4b9173d316a84438a2a85f978530f316850c1
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-01-29 20:36:53 +0200

    Add accessible name to each project repository link

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,14 +2,19 @@ import { ExternalLink, GitHub } from "react-feather";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="border rounded border-nord3 dark:border-opacity-100 border-opacity-50 p-4 w-full font-mono">
+    <div className="w-full p-4 font-mono border border-opacity-50 rounded border-nord3 dark:border-opacity-100">
       <div className="flex justify-between">
         <h2 className="text-2xl dark:text-nord8 text-nord3">
           <a href={project.demo} target="_blank" rel="noreferrer">
             {project.name} <ExternalLink className="inline" />
           </a>
         </h2>
-        <a href={project.repo} target="_blank" rel="noreferrer">
+        <a
+          href={project.repo}
+          target="_blank"
+          rel="noreferrer"
+          aria-label={`link to ${project.name} project`}
+        >
           <GitHub className="inline w-8 h-8" />
         </a>
       </div>
