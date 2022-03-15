45c1db65def2e67ba16c0f6bd08f04afd76b607e
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-15 18:52:05 +0300

    Change headline alignment

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ data }) {
           id="home"
           className="flex flex-col items-center justify-center py-4 border-b-2 border-opacity-50 border-nord3 xl:flex-row"
         >
-          <h1 className="px-16 mb-8 text-5xl font-bold text-center lg:text-7xl 2xl:text-8xl text-gradient bg-nord-gradient selection:bg-opacity-40 2xl:text-left">
+          <h1 className="px-16 mb-8 text-5xl font-bold text-center lg:text-7xl 2xl:text-8xl text-gradient bg-nord-gradient selection:bg-opacity-40 xl:text-left">
             {data.home.heading}
           </h1>
           <div className="flex items-center justify-center gap-4 px-24 text-xl xl:text-2xl xl:gap-6 xl:flex-col xs:flex-col sm:flex-row 2xl:text-4xl dark:text-nord8">
