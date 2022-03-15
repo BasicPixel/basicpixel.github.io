cd84877eb0a604e5ee4abca16eef77940a35a6da
BasicPixel <69857856+BasicPixel@users.noreply.github.com>
2022-03-15 15:52:48 +0000

    Deploying to gh-pages from @ BasicPixel/basicpixel.github.io@45c1db65def2e67ba16c0f6bd08f04afd76b607e 🚀

--- _next/static/Cj2QN09ySM1LqNw3Y0tWF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-34c43dc38a948d36.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
