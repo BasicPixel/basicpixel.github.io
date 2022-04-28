00716469c1647c42d22346c754c65f7863166ba8
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-04-29 00:07:33 +0300

    Small footer refinements

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,10 +1,8 @@
-import { Heart } from "react-feather";
-
 import data from "public/en.json";
 
 const Footer = () => {
   return (
-    <footer className="py-4 font-mono text-center border-t border-nord3 border-opacity-30">
+    <footer className="py-4 font-mono text-center border-t border-nord3 border-opacity-40">
       {data.footer.body[0]}
       <a
         href="https://www.nordtheme.com/"
