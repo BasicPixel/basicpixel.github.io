927797f425e79098df72264bbd6dd57e348d9434
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-04-30 02:45:14 +0300

    Add coming soon text to blog section

--- pages/blog.js
+++ pages/blog.js
@@ -4,6 +4,7 @@ const BlogPage = () => {
   return (
     <PageTransition>
       <h1 className="text-4xl">Blog</h1>
+      <p className="text-xl text-secondary">Coming soon...</p>
     </PageTransition>
   );
 };
