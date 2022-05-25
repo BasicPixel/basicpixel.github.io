5d2170d1eb00b08130d3c5e7491b349f8ca4808e
Osama AlQudah <nightknight218@gmail.com>
2022-05-25 19:21:53 +0300

    Undo dynamic sitemap gen, stick to normal to be able to next export

--- next-sitemap.js
+++ next-sitemap.js
@@ -5,10 +5,4 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://pixel.is-a.dev",
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      `https://pixel.is-a.dev/sitemap.xml`,
-      `https://pixel.is-a.dev/server-sitemap.xml`,
-    ],
-  },
 };
